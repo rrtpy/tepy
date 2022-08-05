b099bfc63e38c0c5b4b5caed4a6b6196ad475c52
sophea <sopheakongs001@gmail.com>
2022-08-05 23:54:00 +0700

    settings

--- ablog/settings.py
+++ ablog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
